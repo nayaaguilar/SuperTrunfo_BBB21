ca7826247b3af3980beddc98980967d7a104238a
Nayya <81198668+nayyadev@users.noreply.github.com>
2021-05-26 10:38:50 -0300

    Add files via upload

---
+++ SuperTrunfo_BBB21/script.js
@@ -0,0 +1,323 @@
+var carta1 = {
+    nome: "Gil do Vigor",
+    imagem: "https://cdn.folhape.com.br/img/pc/1100/1/dn_arquivo/2021/01/whatsapp-image-2021-01-19-at-82050-pm.jpg",
+    atributos: {
+      Inteligência: 95,
+      Engraçado: 95,
+      Jogador: 85
+    }
+  };
+  var carta2 = {
+    nome: "João",
+    imagem:
+      "https://www.maisnovela.com.br/images/large/2021/01/19/participantes-do-bbb21-1132843.jpg",
+    atributos: {
+     Inteligência: 90,
+      Engraçado: 60,
+      Jogador: 80
+    }
+  };
+  var carta3 = {
+    nome: "Karol Conká",
+    imagem:
+      "https://cdn.folhape.com.br/upload/dn_arquivo/2021/01/whatsapp-image-2021-01-19-at-165529.jpeg",
+    atributos: {
+      Inteligência: 50,
+      Engraçado: 20,
+      Jogador: 25
+    }
+  };
+  var carta4 = {
+    nome: "Juliette",
+    imagem:
+      "https://midias.correiobraziliense.com.br/_midias/jpg/2021/01/19/675x450/1_juliette-6489875.jpg",
+    atributos: {
+     Inteligência: 95,
+      Engraçado: 100,
+      Jogador: 100
+    }
+  };
+  var carta5 = {
+    nome: "Lucas Penteado",
+    imagem:
+      "https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4488962:1611093389/Lucas-Penteado.jpeg?f=default&$p$f=4e393af",
+    atributos: {
+    Inteligência: 70,
+      Engraçado: 90,
+      Jogador: 0
+    } 
+  }
+  var carta6 = {
+    nome: "Camilla de Lucas",
+    imagem:
+      "https://i.glbimg.com/og/ig/infoglobo/f/original/2021/01/21/camilla-de-lucas.jpg",
+    atributos: {
+      Inteligência: 85,
+      Engraçado: 90,
+      Jogador: 80
+    } 
+  }
+  var carta7 = {
+    nome: "Nego Di",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/bbb-bbb-21-bbb21-big-brother-brasil-participantes-quem-sao-os-participantes-nego-di.jpg",
+    atributos: {
+     Inteligência: 20,
+      Engraçado: 10,
+      Jogador: 50
+    } 
+  }
+  var carta8 = {
+    nome: "Projota",
+    imagem:
+      "https://midias.correiobraziliense.com.br/_midias/jpg/2021/01/19/projota-6490629.jpg",
+    atributos: {
+      Inteligência: 70,
+      Engraçado: 20,
+      Jogador: 80
+    } 
+  }
+  var carta9 = {
+    nome: "Viih Tube",
+    imagem:
+      "https://www.rbsdirect.com.br/filestore/7/0/6/2_e4b54dc0b2ac3ed/2607_685487da3259390.jpg?w=700",
+    atributos: {
+      Inteligência: 90,
+      Engraçado: 50,
+      Jogador: 100
+    } 
+  }
+  var carta10 = {
+    nome: "Arcrebiano",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/arcrebiano-bill-bbb.jpg",
+    atributos: {
+      Inteligência: 30,
+      Engraçado: 10,
+      Jogador: 0
+    } 
+  }
+  var carta11 = {
+    nome: "Arthur",
+    imagem:
+      "https://noticiasdatv.uol.com.br/media/_versions/bio/arthur-picoli-bbb21-divulgacao-globo_fixed_big.jpg",
+    atributos: {
+      Inteligência: 70,
+      Engraçado: 80,
+      Jogador: 85
+    } 
+  }
+  var carta12 = {
+    nome: "Caio",
+    imagem:
+      "https://seriesemcena.com.br/wp-content/uploads/2021/01/caio.jpg",
+    atributos: {
+      Inteligência: 70,
+      Engraçado: 65,
+      Jogador: 75
+    } 
+  }
+  var carta13 = {
+    nome: "Fiuk",
+    imagem: "https://paisefilhos.uol.com.br/wp-content/uploads/2021/01/projota-1.jpg",
+    atributos: {
+      Inteligência: 50,
+      Engraçado: 30,
+      Jogador: 60
+    } 
+  }
+  var carta14 = {
+    nome: "Carla Diaz",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/bbb-bbb-21-bbb21-big-brother-brasil-participantes-quem-sao-os-participantes-carla-diaz.jpg",
+    atributos: {
+      Inteligência: 70,
+      Engraçado: 40,
+      Jogador: 30
+    } 
+  }
+  var carta15 = {
+    nome: "Pocah",
+    imagem:
+      "https://www.otempo.com.br/image/contentid/policy:1.2437152:1611090907/WhatsApp%20Image%202021-01-19%20at%2018.11.21.jpeg?f=3x2&q=0.6&$p$f$q=8cddd92",
+    atributos: {
+      Inteligência: 40,
+      Engraçado: 55,
+      Jogador: 50
+    } 
+  }
+  var carta16 = {
+    nome: "Thais",
+    imagem:
+      "https://odia.ig.com.br/_midias/jpg/2021/01/19/1200x750/1_assets_fotos_864_thais_1cde7e80107b-20998517.jpg",
+    atributos: {
+      Inteligência: 30,
+      Engraçado: 40,
+      Jogador: 20
+    } 
+  }
+  var carta17 = {
+    nome: "Kerline",
+    imagem:
+      "https://www.rbsdirect.com.br/imagesrc/27665813.jpg?w=1200&h=630&a=c&version=1575255600",
+    atributos: {
+      Inteligência: 30,
+      Engraçado: 0,
+      Jogador: 0
+    } 
+  }
+  var carta18 = {
+    nome: "Lumena",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/bbb-bbb-21-bbb21-big-brother-brasil-participantes-quem-sao-os-participantes-lumena.jpg",
+    atributos: {
+      Inteligência: 75,
+      Engraçado: 30,
+      Jogador: 30
+    } 
+  }
+  var carta19 = {
+    nome: "Sarah",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/sarah-bbb.jpg",
+    atributos: {
+      Inteligência: 80,
+      Engraçado: 70,
+      Jogador: 85
+    } 
+  }
+  var carta20 = {
+    nome: "Rodolffo",
+    imagem:
+      "https://paranaportal.uol.com.br/wp-content/uploads/2021/01/bbb-bbb-21-bbb21-big-brother-brasil-participantes-quem-sao-os-participantes-rodolffo.jpg",
+    atributos: {
+      Inteligência: 50,
+      Engraçado: 60,
+      Jogador: 65
+    } 
+  }
+  var cartaMaquina;
+  var cartaJogador;
+  var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20];
+  
+  var pontosJogador = 0
+  var pontosMaquina = 0
+  
+  atualizaPlacar() 
+  atualizaQuantidadeDeCartas()
+  
+  function atualizaQuantidadeDeCartas() {
+    var divQuantidadeDeCartas = document.getElementById('quantidade-cartas')
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+    divQuantidadeDeCartas.innerHTML = html 
+  }
+  
+  function atualizaPlacar() {
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " x " + pontosMaquina + " Máquina"
+  divPlacar.innerHTML = html
+  }
+  
+  function sortearCarta() {
+      var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+      cartaMaquina = cartas[numeroCartaMaquina]
+      cartas.splice(numeroCartaMaquina, 1)
+  
+      var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+      cartaJogador = cartas[numeroCartaJogador]
+      cartas.splice(numeroCartaJogador, 1)
+    
+      document.getElementById('btnSortear').disabled = true
+      document.getElementById('btnJogar').disabled = false
+  
+      exibeCartaJogador()
+  }
+  
+  function exibeCartaJogador() {
+      var divCartaJogador = document.getElementById("carta-jogador")
+      var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+      divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+      var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+      var opcoesTexto = ""
+  
+      for (var atributo in cartaJogador.atributos) {
+          opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+      }
+  
+      var html = "<div id='opcoes' class='carta-status'>"
+  
+      divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+  }
+  
+  function obtemAtributoSelecionado() {
+      var radioAtributo = document.getElementsByName('atributo')
+      for (var i = 0; i < radioAtributo.length; i++) {
+          if (radioAtributo[i].checked) {
+              return radioAtributo[i].value
+          }
+      }
+  }
+  
+  function jogar() {
+      var divResultado = document.getElementById("resultado")
+      var atributoSelecionado = obtemAtributoSelecionado()
+  
+      if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+          htmlResultado = '<p class="resultado-final">Venceu</p>'
+          pontosJogador++
+      } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+          htmlResultado = '<p class="resultado-final">Perdeu</p>'
+          pontosMaquina++
+      } else {
+          htmlResultado = '<p class="resultado-final">Empatou</p>'
+      }
+      if (cartas.length == 0) {
+        alert("Fim de Jogo")
+        if(pontosJogador > pontosMaquina) {
+          htmlResultado = '<p class="resultado-final">Você ganhou!</p>'
+        } else if (pontosMaquina > pontosJogador) {
+          htmlResultado = '<p class="resultado-final">Perdeu!</p>'
+        } else {
+          htmlResultado = '<p class="resultado-final">Empatou!</p>'
+  }
+      } else {
+        document.getElementById('btnProximaRodada').disabled = false
+      }
+  
+      divResultado.innerHTML = htmlResultado
+      document.getElementById('btnJogar').disabled = true
+     
+      exibeCartaMaquina()
+      atualizaPlacar()
+      atualizaQuantidadeDeCartas()
+  }
+  
+  function exibeCartaMaquina() {
+      var divCartaMaquina = document.getElementById("carta-maquina")
+      var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+      divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+      var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+      var opcoesTexto = ""
+  
+      for (var atributo in cartaMaquina.atributos) {
+          console.log(atributo)
+          opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+      }
+  
+      var html = "<div id='opcoes' class='carta-status --spacing'>"
+  
+      divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+  }
+  
+  function proximaRodada() {
+    var divCartas = document.getElementById('cartas')
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+    
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled - true
+    document.getElementById('btnProximaRodada').disabled - true
+    
+    var divResultado = document.getElementById('resultado')
+    
+    divResultado.innerHTML = ""
+  }
